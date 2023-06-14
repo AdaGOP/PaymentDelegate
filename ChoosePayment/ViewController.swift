6098080bae936669447ce83af4938f06b3adc12c
Allicia Viona <alliciawn@gmail.com>
2023-06-14 09:47:13 +0700

    Initial Commit

---
+++ ChoosePayment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChoosePayment
+//
+//  Created by Allicia Viona Sagi on 14/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
